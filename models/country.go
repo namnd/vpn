028e4a8b1b3a9e813bdc6adcc215a639024c0167
Nam Nguyen <me@namnd.com>
2026-03-10 21:57:31 +1000

    fix country name remove space

--- models/country.go
+++ models/country.go
@@ -12,12 +12,12 @@ type Country struct {
 	Nodes []Node
 }
 
-var CountriesInOrder = []string{"New Zealand", "Singapore", "Japan", "India", "US"}
+var CountriesInOrder = []string{"NewZealand", "Singapore", "Japan", "India", "US"}
 
 var CountryRegion = map[string]string{
-	"New Zealand": "ap-southeast-6",
-	"Singapore":   "ap-southeast-1",
-	"Japan":       "ap-northeast-1",
-	"India":       "ap-south-1",
-	"US":          "us-east-1",
+	"NewZealand": "ap-southeast-6",
+	"Singapore":  "ap-southeast-1",
+	"Japan":      "ap-northeast-1",
+	"India":      "ap-south-1",
+	"US":         "us-east-1",
 }
